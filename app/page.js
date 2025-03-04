f65b62c1972426a9121f0f5c9a65f1ee7c6c42f3
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-03-04 00:27:51 +0000

    logo title

--- app/page.js
+++ app/page.js
@@ -1,19 +1,18 @@
 "use client";
- 
 
 import Paws from "./components/paws";
 
 export default function Home() {
   return (
-    <div  
-    className="w-screen min-h-screen"
-    style={{
-      background: "linear-gradient(135deg, #A1B28D 25%, #B5A888 25%, #B5A888 50%, #A1B28D 50%, #9A9A7B 75%, #B5A888 75%)",
-      backgroundSize: "100% 100%",
-      color: '#333', // Keep text color professional
-    }}
-    >
-      <img src="../paw-space-nb.png" alt="Paw Print" className="left-0 h-16 w-16 md:h-32 md:w-32" />
+    <div className="w-screen min-h-screen relative flex flex-col items-center bg-gradient-to-br from-[#A1B28D] via-[#B5A888] to-[#9A9A7B]">
+      <img src="../paw-space-nb.png" alt="Paw Print" className=" top-20 h-50 w-50 md:h-100 md:w-100" />
+
+      {/* BIG WALKS positioned bottom center */}
+      <h1 className="absolute top-28 md:top-60 left-1/2 -translate-x-1/2 text-[#B5A888] font-black text-3xl md:text-6xl tracking-wide flex flex-col text-center drop-shadow-[0_0_10px_rgba(0,0,0,0.8)]">
+        BIG 
+        <span>WALKS</span>
+      </h1>
+
       <Paws />
     </div>
   );
