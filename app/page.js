15030f9ceedbd02e4bc70b89088181aa47bf24ef
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-03-04 02:43:42 +0000

    logo size reduced

--- app/page.js
+++ app/page.js
@@ -10,10 +10,10 @@ export default function Home() {
     
     <div className="w-screen min-h-screen flex flex-col items-center bg-gradient-to-br from-[#A1B28D] via-[#B5A888] to-[#9A9A7B]">
       
-      <div><img src="../paw-space-nb.png" alt="Paw Print" className="top-20 h-50 w-50 md:h-70 md:w-70" />
+      <div><img src="../paw-space-nb.png" alt="Paw Print" className="top-20 h-50 w-50" />
 
       
-      <h1 className="absolute top-28 md:top-42    left-1/2 -translate-x-1/2 md:-translate-x-[52%] text-[#B5A888] brightness-125 font-black text-3xl md:text-5xl tracking-wide flex flex-col text-center drop-shadow-[0_0_10px_rgba(0,0,0,0.8)]">
+      <h1 className="absolute top-28 left-1/2 -translate-x-1/2 text-[#B5A888] brightness-125 font-black text-3xl tracking-wide flex flex-col text-center drop-shadow-[0_0_10px_rgba(0,0,0,0.8)]">
         BIG 
         <span>WALKS</span>
       </h1>
