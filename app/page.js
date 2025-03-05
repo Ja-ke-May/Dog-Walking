6570b8812dacbbdeb295b2ec8e8a4b7c1e372b70
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-03-05 02:03:44 +0000

    pre load images

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 "use client";
 
+import Head from 'next/head';
 import Paws from "./components/paws";
 import Content from "./components/content";
 
 export default function Home() {
-
   const images = {
     intro: [
       {
@@ -45,35 +45,35 @@ export default function Home() {
       }
     ]
   };
-  
-
 
   return (
     <>
-    <Paws />
-    
-    <div className="w-full h-screen flex flex-col items-center bg-gradient-to-br from-[#A1B28D] via-[#B5A888] to-[#9A9A7B]">
-      
-      <img src="../Logo-no-bg.png" alt="Paw Print" className="top-20 min-h-[20%] max-h-[30%] " />
+      <Head>
+        {/* Preload images */}
+        <link rel="preload" href="../on-log.jpeg" as="image" />
+        <link rel="preload" href="../on-log-2.jpeg" as="image" />
+        <link rel="preload" href="pup-patch.png" as="image" />
+        <link rel="preload" href="/Butch.jpg" as="image" />
+        <link rel="preload" href="/Candy.jpg" as="image" />
+        <link rel="preload" href="Jacob May.jpg" as="image" />
+      </Head>
 
-      
-      <h1 className="hidden absolute top-28 left-1/2 -translate-x-1/2 text-[#B5A888] brightness-125 font-black text-3xl flex flex-col text-center">
-        BIG 
-        <span>WALKS</span>
-      </h1>
+      <Paws />
 
-      
-      
-      <Content 
-      images={images}
-      />
-      
+      <div className="w-full h-screen flex flex-col items-center bg-gradient-to-br from-[#A1B28D] via-[#B5A888] to-[#9A9A7B]">
+        <img
+          src="../Logo-no-bg.png"
+          alt="Paw Print"
+          className="top-20 min-h-[20%] max-h-[30%]"
+        />
 
+        <h1 className="hidden absolute top-28 left-1/2 -translate-x-1/2 text-[#B5A888] brightness-125 font-black text-3xl flex flex-col text-center">
+          BIG
+          <span>WALKS</span>
+        </h1>
 
-      
-
-    </div>
-
+        <Content images={images} />
+      </div>
     </>
   );
 }
