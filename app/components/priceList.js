3cfd2ddf4008cb1b199ec2c7db28c9004c2f0120
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-03-05 03:01:21 +0000

    hr line price

--- app/components/priceList.js
+++ app/components/priceList.js
@@ -17,21 +17,21 @@ const PriceList = ({ images }) => {
         {/* Pricing */}
         <div className="flex-grow grid grid-cols-2 gap-4">
           {/* 45m standard walk £20 */}
-          <div className="flex items-center justify-center p-4 bg-black/80 text-[#B5A888] brightness-125 rounded md:text-lg lg:text-xl">
+          <div className="flex items-center justify-center p-4 bg-black/80 text-[#B5A888] brightness-125 rounded md:text-lg">
             <p className="whitespace-normal text-center">
               <span className=""><span className="font-bold">45m </span><br />Big Walk <hr className="mt-2" /><span className="font-bold">£20</span></span>
             </p>
           </div>
           
           {/* 90m extended walk £30 */}
-          <div className="flex items-center justify-center p-4 bg-black/80 text-[#B5A888] brightness-125 rounded md:text-lg lg:text-xl">
+          <div className="flex items-center justify-center p-4 bg-black/80 text-[#B5A888] brightness-125 rounded md:text-lg">
             <p className="whitespace-normal text-center">
               <span className=""><span className="font-bold">90m</span> <br /> Huge Walk <hr className="mt-2" /><span className="font-bold">£30</span></span>
             </p>
           </div>
           
           {/* 3/4h Half Day Adventure £60 */}
-          <div className="flex items-center justify-center p-4 bg-black/80 text-[#B5A888] brightness-125 rounded md:text-lg lg:text-xl">
+          <div className="flex items-center justify-center p-4 bg-black/80 text-[#B5A888] brightness-125 rounded md:text-lg">
             <p className="whitespace-normal text-center">
               
               <span className=""><span className="font-bold">3/4h</span> Hike <hr className="mt-2" /><span className="font-bold">£60</span></span>
@@ -39,7 +39,7 @@ const PriceList = ({ images }) => {
           </div>
           
           {/* 10m House Visits £10 */}
-          <div className="flex items-center justify-center p-4 bg-black/80 text-[#B5A888] brightness-125 rounded md:text-lg lg:text-xl">
+          <div className="flex items-center justify-center p-4 bg-black/80 text-[#B5A888] brightness-125 rounded md:text-lg">
             <p className="whitespace-normal text-center">
               <span className=""><span className="font-bold">10m</span> Visit <hr className="mt-2" /><span className="font-bold">£10</span></span>
             </p>
