d91565781bec35c2bfe47d7bc95d3a9cbdef954a
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-03-06 00:21:31 +0000

    fixed paws

--- app/page.js
+++ app/page.js
@@ -96,7 +96,7 @@ export default function Home() {
         <img
           src="../BIG_WALKS_green_brown_bg-removebg.png"
           alt="Paw Print"
-          className="fixed top-0 min-h-[20%] max-h-[30%]"
+          className="fixed top-0 min-h-[15%] max-h-[25%]"
         />
 
         <h1 className="hidden absolute top-28 left-1/2 -translate-x-1/2 text-[#B5A888] brightness-125 font-black text-3xl flex flex-col text-center">
